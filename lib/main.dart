5d5f8d0104df78f1907238f9b503e88c0111d023
pepix <pepix2112@gmail.com>
2018-03-13 02:14:55 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var title = "Web Images";
+
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Image.network("https://i.ytimg.com/vi/tMm7cr0ULwE/maxresdefault.jpg"),
+      ),
+    );
+  }
+}
