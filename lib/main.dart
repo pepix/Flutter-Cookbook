e1475f5837f615d877b90d44194f8102977ae8e6
pepix <pepix2112@gmail.com>
2018-03-13 03:13:26 +0900

    add cache function

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(new MyApp());
 
@@ -19,19 +19,27 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text(title),
         ),
+        body: new Center(
+          child: new CachedNetworkImage(
+              placeholder: new CircularProgressIndicator(),
+              imageUrl: "https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true",
+          ),
+        ),
+        /*
         body: new Stack(
           children: <Widget>[
             new Center(
               child: new CircularProgressIndicator(),
             ),
             new Center(
-              child: new FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  image: "https://i.ytimg.com/vi/tMm7cr0ULwE/maxresdefault.jpg",
+              child: new CachedNetworkImage (
+                  placeholder: new CircularProgressIndicator(),
+                  imageUrl: "https://i.ytimg.com/vi/tMm7cr0ULwE/maxresdefault.jpg",
               ),
             ),
           ],
         ),
+        */
       ),
     );
   }
