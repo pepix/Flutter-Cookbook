a39a70f44cbc0dfd9135f7f4455b77fd7f2730df
pepix <pepix2112@gmail.com>
2018-03-15 01:23:47 +0900

    simulate in the android simulator

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,32 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text(title),
         ),
+        body: new GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 8.0, // ColumnSpace
+            crossAxisSpacing: 8.0, // RowSpace
+            childAspectRatio: 1.0,
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              new Container(
+                color: Colors.red,
+              ),
+              new Container(
+                color: Colors.blue,
+              ),
+              new Container(
+                color: Colors.yellow,
+              ),
+            ],
+        ),
+        /*
         body: new Center(
           child: new CachedNetworkImage(
               placeholder: new CircularProgressIndicator(),
               imageUrl: "https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true",
           ),
         ),
+        */
         /*
         body: new Stack(
           children: <Widget>[
